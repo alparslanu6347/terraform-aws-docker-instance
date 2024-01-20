0d734156c706cce9f275d578bb785850442f02d5
alparslanu6347 <alparslanu6347@gmail.com>
2024-01-20 23:58:03 +0300

    version.tf updated

--- userdata.sh
+++ userdata.sh
@@ -8,3 +8,5 @@ usermod -a -G docker ec2-user
 # install docker-compose
 curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+## Amazon Linux 2023 , Docker & Docker-compose 
